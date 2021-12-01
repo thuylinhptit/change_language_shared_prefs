b1c7bf1a0f47ce530f2cff444f8bd1a7d5a3f5b9
thuylinhptit <thuylinhptit1010@gmail.com>
2021-12-01 13:59:03 +0700

    update load image

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -6,25 +5,24 @@ import 'package:image_picker/image_picker.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:test_theme/local_string.dart';
-import 'package:test_theme/shared_prefs.dart';
 import 'package:test_theme/theme_provider.dart';
 
-void main() {
+void main() async {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
       create: (_) => ThemeNotifier(),
       child: Consumer<ThemeNotifier>(
         builder: (context, ThemeNotifier notifier, child) {
-          print("check: ");
-         // print(notifier.vnLanguage);
-          print(notifier.darkTheme);
           return GetMaterialApp(
-            locale: notifier.vnLanguage ?  Locale('vi','VN'):  Locale('en','US'),
+            locale:
+                notifier.vnLanguage ? Locale('vi', 'VN') : Locale('en', 'US'),
             translations: LocalString(),
             theme: notifier.darkTheme ? ThemeData.dark() : ThemeData.light(),
             home: SettingsPage(),
@@ -36,118 +34,131 @@ class MyApp extends StatelessWidget {
 }
 
 class SettingsPage extends StatefulWidget {
+  const SettingsPage({Key? key}) : super(key: key);
+
   @override
   _SettingsPage createState() => _SettingsPage();
 }
 
+String? _imagePath;
+
 class _SettingsPage extends State<SettingsPage> {
   ThemeNotifier themeNotifier = ThemeNotifier();
-  File? imageFile;
-
-  loadImageFromPrefs() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    final imageKeyValue = prefs.getString(IMAGE_KEY);
-    if (imageKeyValue != null) {
-      final imageString = await ImageSharedPrefs.loadImageFromPrefs();
-      setState(() {
-        imageFile = ImageSharedPrefs.imageFrom64BaseString(imageString!);
-      });
-    }
-  }
+  File? _image;
 
   @override
   void initState() {
     super.initState();
-    loadImageFromPrefs();
+    loadImage();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title:  Text('change_language'.tr),
+          title: Text('change_language'.tr),
         ),
         drawer: Drawer(
             child: ListView(
-              children: [
-                ListTile(
-                    title:  Text('change_bg'.tr, style: const TextStyle( fontSize: 18),),
-                    onTap:(){
-                      _getFromGallery();
-                    }
+          children: [
+            ListTile(
+                title: Text(
+                  'change_bg'.tr,
+                  style: const TextStyle(fontSize: 18),
                 ),
-                Consumer<ThemeNotifier>(
-                  builder:(context, notifier, child) =>
-                      ListTile(
-                        title: Row(
-                          children: [
-                            Text('change_mode'.tr, style: const TextStyle(  fontSize: 18),),
-                            const Spacer(),
-                            Switch(
-                              onChanged:(value){
-                                notifier.toggleTheme();
-                                print(notifier.darkTheme);
-                              },
-                              value: notifier.darkTheme ,
-                            ),
-                          ],
-                        ),
-                      ),
+                onTap: () {
+                  PickImage();
+                }),
+            Consumer<ThemeNotifier>(
+              builder: (context, notifier, child) => ListTile(
+                title: Row(
+                  children: [
+                    Text(
+                      'change_mode'.tr,
+                      style: const TextStyle(fontSize: 18),
+                    ),
+                    const Spacer(),
+                    Switch(
+                      onChanged: (value) {
+                        notifier.toggleTheme();
+                      },
+                      value: notifier.darkTheme,
+                    ),
+                  ],
                 ),
-                Consumer<ThemeNotifier>(
-                    builder: (context,notifier, child) =>
-                        ListTile(
-                          title: Row(
-                            children:  [
-                              Text('change_language'.tr, style: const TextStyle(  fontSize: 18),),
-                              const Spacer(),
-                              GestureDetector(
-                                child: Container(
-                                  width: 40,
-                                  decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(10),
-                                  ),
-                                  child: notifier.vnLanguage == true ? Image.asset('assets/flat_vn.png',) : Image.asset('assets/flat_en.png',),
-                                ),
-                                onTap: (){
-                                  notifier.changeLanguage();
-                                  if( notifier.vnLanguage == true ){
-                                    Get.updateLocale(const Locale('vi','VN'));
-                                  }
-                                  else{
-                                    Get.updateLocale(const Locale('en','US'));
-                                  }
-                                },
-                              )
-                            ],
+              ),
+            ),
+            Consumer<ThemeNotifier>(
+                builder: (context, notifier, child) => ListTile(
+                      title: Row(
+                        children: [
+                          Text(
+                            'change_language'.tr,
+                            style: const TextStyle(fontSize: 18),
                           ),
-                        )
-                )
-              ],
-            )
-        ),
+                          const Spacer(),
+                          GestureDetector(
+                            child: Container(
+                              width: 40,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              child: notifier.vnLanguage == true
+                                  ? Image.asset(
+                                      'assets/flat_vn.png',
+                                    )
+                                  : Image.asset(
+                                      'assets/flat_en.png',
+                                    ),
+                            ),
+                            onTap: () {
+                              notifier.changeLanguage();
+                              if (notifier.vnLanguage == true) {
+                                Get.updateLocale(const Locale('vi', 'VN'));
+                              } else {
+                                Get.updateLocale(const Locale('en', 'US'));
+                              }
+                            },
+                          )
+                        ],
+                      ),
+                    ))
+          ],
+        )),
         body: Container(
-            child: imageFile == null
-                ? Container(
-            ): Image.file(
-              imageFile!,
-              fit: BoxFit.cover,
-            )
-        )
-    );
+            decoration: BoxDecoration(
+                image: _imagePath != null
+                    ? DecorationImage(
+                        image: FileImage(
+                          File(_imagePath!),
+                        ),
+                        fit: BoxFit.fill)
+                    : null),
+            child: _imagePath == null
+                ? Container()
+                : Image(
+                    image: FileImage(File(_imagePath!)),
+                  )));
   }
-  _getFromGallery() async {
-    PickedFile? pickedFile = await ImagePicker().getImage(
-      source: ImageSource.gallery,
-      maxWidth: 1800,
-      maxHeight: 1800,
-    );
-    if (pickedFile != null) {
-      setState(() {
-        imageFile = File(pickedFile.path);
-      });
-      ImageSharedPrefs.saveImageToPrefs(
-          ImageSharedPrefs.base64String(imageFile!.readAsBytesSync()));
-    }
+
+  void PickImage() async {
+    var image = await ImagePicker().getImage(source: ImageSource.gallery);
+    setState(() {
+      _image = File(image!.path);
+      _imagePath = _image!.path;
+    });
+    SaveImage(_image!.path);
+  }
+
+  void SaveImage(path) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setString("save", path);
+  }
+
+  void loadImage() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _imagePath = prefs.getString("save")!;
+    });
   }
 }
